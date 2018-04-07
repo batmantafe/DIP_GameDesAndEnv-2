603aa153a678c9c4167d2c4a8d06085464293663
Duane Reymond <duane.reymond@gmail.com>
2018-04-07 17:34:33 +1000

    First commit. Rooms 1 and 2, and Elevator 1 greybox from Level Layout design.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shortcuts();
+    }
+
+    void Shortcuts()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
