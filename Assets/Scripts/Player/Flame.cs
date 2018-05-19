0231fd9fe75b65a0351e331885ae84662480c500
Duane Reymond <duane.reymond@gmail.com>
2018-05-19 15:48:00 +1000

    Added Flame script and Torches script.

---
+++ Assets/Scripts/Player/Flame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour
+{
+    public Light flameLight;
+
+    public float flameStartRange, flameStartIntensity;
+
+    public float fuelMax, fuelCurrent, burnRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartConditions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BurnDown();
+    }
+
+    void StartConditions()
+    {
+        flameLight = GetComponent<Light>();
+
+        fuelMax = 100f;
+        fuelCurrent = fuelMax;
+        burnRate = 30f;
+
+        flameStartRange = flameLight.range;
+        flameStartIntensity = flameLight.intensity;
+    }
+
+    void BurnDown()
+    {
+        fuelCurrent = fuelCurrent - (fuelMax / burnRate * Time.deltaTime);
+
+        //Debug.Log(fuelCurrent);
+
+        if (fuelCurrent > 0f)
+        {
+            flameLight.range = flameLight.range - (flameStartRange / burnRate * Time.deltaTime);
+            flameLight.intensity = flameLight.intensity - (flameStartIntensity / burnRate * Time.deltaTime);
+        }
+
+        if (fuelCurrent <= 0f)
+        {
+            fuelCurrent = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Torches.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torches : MonoBehaviour
+{
+
+    public Light torchLight;
+
+    public float torchStartRange, torchStartIntensity;
+
+    public float fuelMax, fuelCurrent, burnRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartConditions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BurnDown();
+    }
+
+    void StartConditions()
+    {
+        torchLight = GetComponent<Light>();
+
+        fuelMax = 100f;
+        fuelCurrent = fuelMax;
+        burnRate = 10f;
+
+        torchStartRange = torchLight.range;
+        torchStartIntensity = torchLight.intensity;
+    }
+
+    void BurnDown()
+    {
+        torchLight.range = torchLight.range - (torchStartRange / burnRate * Time.deltaTime);
+        torchLight.intensity = torchLight.intensity - (torchStartIntensity / burnRate * Time.deltaTime);
+    }
+}
