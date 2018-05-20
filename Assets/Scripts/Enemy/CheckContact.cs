f3ae0f890e22c06f8ab1cc972d89c40105dc5aa5
Duane Reymond <duane.reymond@gmail.com>
2018-05-20 17:06:49 +1000

    Player loses when Enemy touches Player.

---
+++ Assets/Scripts/Enemy/CheckContact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckContact : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("Enemy touched Player");
+
+            gameManager.playerLost = true;
+        }
+    }
+}
