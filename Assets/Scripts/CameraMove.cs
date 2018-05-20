8b5443b9fd8367ee3a48e61fa8464a1d7e8de909
Duane Reymond <duane.reymond@gmail.com>
2018-05-20 21:45:42 +1000

    Polish on Camera in Menu scene.

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform camTarget;
+
+    public float camSpeed;
+
+    public GameObject blockLight;
+
+    // Use this for initialization
+    void Start()
+    {
+        camSpeed = .1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, camTarget.position, camSpeed * Time.deltaTime);
+
+        if (transform.position == camTarget.position)
+        {
+            blockLight.SetActive(true);
+        }
+    }
+}
