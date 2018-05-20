1f5da4986981c33108be388b9447820e2d9782e6
Duane Reymond <duane.reymond@gmail.com>
2018-05-20 15:17:36 +1000

    Added a closing Block for the Sky.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Transform blockTarget;
+
+    public float blockSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        blockSpeed = .0067f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, blockTarget.position, blockSpeed * Time.deltaTime);
+    }
+}
