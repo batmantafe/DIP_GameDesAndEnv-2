90119096e2ddd564cfd354afb76e5d13993391b0
Duane Reymond <duane.reymond@gmail.com>
2018-05-20 12:29:16 +1000

    Enemy script added. Enemy moves toward Player on NavMesh.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+
+    private NavMeshAgent nav;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            nav.SetDestination(player.position);
+        }
+    }
+}
